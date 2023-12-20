e8ded52a68c8d49762778636aeb920cd26fa8960
Killian Barbarin <killianb31@gmail.com>
2023-12-20 11:09:27 +0100

    ADD a chance to modify actual element in the dropzone

--- src/hooks/useDrop.js
+++ src/hooks/useDrop.js
@@ -9,34 +9,47 @@ const useDrop = () => {
         e.preventDefault();
     };
 
-    const handleDrop = (e, dropZone) => {
-        e.preventDefault();
-        if (sourceItem === 'dropzone' && dropZone === 'deletezone') {
-            setItemList(itemList.filter((_, i) => i !== indexItem));
-            setDraggedItem(null); // Réinitialisez l'élément déplacé
-            setIndexItem(null);
-        } else if (sourceItem === 'draggableZone' && dropZone === 'dropzone') {
-            // Ajoutez l'élément déplacé à itemList ou traitez-le comme nécessaire
-
-            const dropZoneRect = e.currentTarget.getBoundingClientRect();
+    const getCoordonate = (e) => {
+        const dropZoneRect = e.currentTarget.getBoundingClientRect();
             const x = e.clientX - dropZoneRect.left; // Coordonnée X relative à la DropZone
             const y = e.clientY - dropZoneRect.top;  // Coordonnée Y relative à la DropZone
             const cellWidth = dropZoneRect.width / 12;
             const cellHeight = dropZoneRect.height / 12;
             const gridColumn = Math.ceil(x / cellWidth);
             const gridRow = Math.ceil(y / cellHeight);
+            return {gridColumn, gridRow}
+    }
 
-            console.log('Column = ' + gridColumn);
-            console.log('Row = ' + gridRow);
-
+    const handleDrop = (e, dropZone) => {
+        e.preventDefault();
+        if (sourceItem === 'dropzone' && dropZone === 'deletezone') {
+            setItemList(itemList.filter((_, i) => i !== indexItem));
+            setDraggedItem(null);
+            setIndexItem(null);
+        } else if (sourceItem === 'draggableZone' && dropZone === 'dropzone') {
+            const {gridColumn, gridRow} = getCoordonate(e);
             const newItem = { ...draggedItem, gridColumn, gridRow };
-
             setItemList([...itemList, newItem]);
             setIndexItem(null);
-            setDraggedItem(null); // Réinitialisez l'élément déplacé
+            setDraggedItem(null);
+        } else if (sourceItem === 'dropzone' && dropZone === 'dropzone') {
+            const {gridColumn, gridRow} = getCoordonate(e);
+
+            const updatedItemList = itemList.map((item, idx) => {
+                if (idx === indexItem) {
+                    // Créez une copie de l'objet item et mettez à jour les propriétés nécessaires
+                    return { ...item, gridRow: gridRow, gridColumn: gridColumn };
+                }
+                return item;
+            });
+        
+            setItemList(updatedItemList);
+
+            setIndexItem(null);
+            setDraggedItem(null);
         } else {
             setIndexItem(null);
-            setDraggedItem(null); // Réinitialisez l'élément déplacé
+            setDraggedItem(null);
         }
     };
 
